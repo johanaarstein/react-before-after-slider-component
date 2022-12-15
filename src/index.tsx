1a73bc39551ad4eba316c4919c317c3d2a6cb569
Johan Martin Aarstein <johan.aarstein@gmail.com>
2022-12-15 23:01:35 +0100

    bump esversion

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,24 @@
-import React, {MouseEventHandler, TouchEventHandler, useEffect, useRef, useState} from 'react';
+import React, {
+    MouseEventHandler,
+    TouchEventHandler,
+    useEffect,
+    useRef,
+    useState
+} from 'react'
 
-import './styles.scss';
+import './styles.scss'
 
 export function isIntersectionObserverSupport() {
-    if (typeof window === 'undefined') return false;
-    return Boolean(window.IntersectionObserver);
+    if (typeof window === 'undefined') return false
+    return Boolean(window.IntersectionObserver)
 }
 
 export interface Image {
-    imageUrl: string;
+    imageUrl: string
     alt?: string,
 }
 
-type OnSliderLoadCallback = () => void;
+type OnSliderLoadCallback = () => void
 
 enum MODE {
     MOVE = 'move',
@@ -38,66 +44,66 @@ function useReadyStatus(
     refContainer: React.RefObject<HTMLDivElement>,
     onReady?: OnSliderLoadCallback
 ) {
-    const [isReady, setIsReady] = useState(false);
+    const [isReady, setIsReady] = useState(false)
 
-    const [imagesLoadedCount, setImagesLoadedCount] = useState(0);
+    const [imagesLoadedCount, setImagesLoadedCount] = useState(0)
     const incrementLoadedImagesCount = () => {
-        setImagesLoadedCount(imagesLoadedCount + 1);
+        setImagesLoadedCount(imagesLoadedCount + 1)
     }
 
     useEffect(() => {
         if (!isReady && imagesLoadedCount === 2 && imagesWidth && refContainer.current) {
-            setIsReady(true);
+            setIsReady(true)
         }
-    }, [imagesLoadedCount, imagesWidth, isReady, refContainer.current]);
+    }, [imagesLoadedCount, imagesWidth, isReady, refContainer.current])
 
     useEffect(() => {
         if(isReady && onReady) {
-            onReady();
+            onReady()
         }
-    }, [isReady]);
+    }, [isReady])
 
     return  {
         onImageLoad: incrementLoadedImagesCount,
         isReady,
-    };
+    }
 }
 
 function useInit(updateContainerWidth: () => void, onMouseUpHandler: () => void) {
     useEffect(() => {
-        updateContainerWidth();
-        document.addEventListener('click', onMouseUpHandler);
+        updateContainerWidth()
+        document.addEventListener('click', onMouseUpHandler)
         return () => {
-            document.removeEventListener('click', onMouseUpHandler);
+            document.removeEventListener('click', onMouseUpHandler)
         }
-    }, []);
+    }, [])
 }
 
 function useResizeFeel(callback: () => void, withResizeFeel?: boolean) {
     useEffect(() => {
         if (withResizeFeel) {
-            window.addEventListener('resize', callback);
+            window.addEventListener('resize', callback)
         }
 
         return () => {
-            window.removeEventListener('resize', callback);
+            window.removeEventListener('resize', callback)
         }
-    }, []);
+    }, [])
 }
 
 function normalizeNewPosition(newPosition: number, imagesWidth: number) {
     if (newPosition > imagesWidth) {
-        return imagesWidth;
+        return imagesWidth
     }
     if (newPosition < 0) {
-        return 0;
+        return 0
     }
 
-    return newPosition;
+    return newPosition
 }
 
-const DEFAULT_START_PERCENT = 50;
-const DEFAULT_BACKGROUND_COLOR = '#fff';
+const DEFAULT_START_PERCENT = 50
+const DEFAULT_BACKGROUND_COLOR = '#fff'
 
 export default function BeforeAfterSlider({
     firstImage,
@@ -112,125 +118,125 @@ export default function BeforeAfterSlider({
     feelsOnlyTheDelimiter = false,
     delimiterColor = DEFAULT_BACKGROUND_COLOR,
 }: Props) {
-    const classNames = ['before-after-slider'];
-    className && classNames.push(className);
+    const classNames = ['before-after-slider']
+    className && classNames.push(className)
 
-    const refContainer = useRef<HTMLDivElement>(null);
-    const [imagesWidth, setImagesWidth] = useState<number | null>(null);
+    const refContainer = useRef<HTMLDivElement>(null)
+    const [imagesWidth, setImagesWidth] = useState<number | null>(null)
     const [delimiterPercentPosition, setDelimiterPosition] = useState(
         currentPercentPosition
         || DEFAULT_START_PERCENT
-    );
-    const [sliderMode, setSliderMode] = useState<MODE>(MODE.DEFAULT);
-    const {onImageLoad, isReady} = useReadyStatus(imagesWidth, refContainer, onReady);
+    )
+    const [sliderMode, setSliderMode] = useState<MODE>(MODE.DEFAULT)
+    const {onImageLoad, isReady} = useReadyStatus(imagesWidth, refContainer, onReady)
     const [containerPosition, setContainerPosition] = useState({
         top: 0,
         left: 0,
-    });
+    })
 
     const onFirstImageLoad = () => {
-        updateContainerWidth();
-        onImageLoad();
+        updateContainerWidth()
+        onImageLoad()
     }
 
     /**
      * Observer start
      */
-    const observerVisiblePercent = 0.95;
+    const observerVisiblePercent = 0.95
     const observerOptions = {
         threshold: [0.0, observerVisiblePercent],
-    };
+    }
     const observerCallback = function(entries: IntersectionObserverEntry[]) {
-        if (!observer || !onVisible) return;
+        if (!observer || !onVisible) return
         entries.forEach(entry => {
             if (entry.intersectionRatio > observerVisiblePercent) {
-                observer.disconnect();
-                onVisible();
+                observer.disconnect()
+                onVisible()
             }
-        });
-    };
+        })
+    }
     const [observer] = useState(
         onVisible && isIntersectionObserverSupport()
             ? new IntersectionObserver(observerCallback, observerOptions)
             : null
-    );
+    )
     useEffect(() => {
         if (observer) {
-            if (!isReady) return;
-            observer.observe(refContainer.current as HTMLDivElement);
+            if (!isReady) return
+            observer.observe(refContainer.current as HTMLDivElement)
         }
-    }, [isReady]);
+    }, [isReady])
     /**
      * Observer end
      */
 
     useEffect(() => {
         if (!currentPercentPosition || !imagesWidth) {
-            return;
+            return
         }
-        setDelimiterPosition(normalizeNewPosition(currentPercentPosition, imagesWidth));
-    }, [currentPercentPosition, imagesWidth]);
+        setDelimiterPosition(normalizeNewPosition(currentPercentPosition, imagesWidth))
+    }, [currentPercentPosition, imagesWidth])
 
     const updateContainerWidth = () => {
-        if (!refContainer.current) return;
-        const containerWidth = refContainer.current.offsetWidth;
-        setImagesWidth(containerWidth);
+        if (!refContainer.current) return
+        const containerWidth = refContainer.current.offsetWidth
+        setImagesWidth(containerWidth)
     }
 
     const onMouseUpHandler = () => {
-        setSliderMode(MODE.DEFAULT);
+        setSliderMode(MODE.DEFAULT)
     }
 
-    useInit(updateContainerWidth, onMouseUpHandler);
+    useInit(updateContainerWidth, onMouseUpHandler)
 
-    const imgStyles = !imagesWidth ? undefined : {width: `${imagesWidth}px`};
-    const secondImgContainerStyle = {width: `${delimiterPercentPosition}%`};
+    const imgStyles = !imagesWidth ? undefined : {width: `${imagesWidth}px`}
+    const secondImgContainerStyle = {width: `${delimiterPercentPosition}%`}
 
     const preparedDelimiterIconStyles = React.useMemo(() => ({
         backgroundColor: delimiterColor,
         ...(delimiterIconStyles ? delimiterIconStyles : {}),
-    }), [delimiterColor, delimiterIconStyles]);
+    }), [delimiterColor, delimiterIconStyles])
 
     const delimiterStyle = React.useMemo(() => ({
         left: `${delimiterPercentPosition}%`,
         backgroundColor: delimiterColor,
-    }), [delimiterPercentPosition, delimiterColor]);
+    }), [delimiterPercentPosition, delimiterColor])
 
     const updateContainerPosition = () => {
-        if (!refContainer.current) return;
-        const containerCoords = refContainer.current.getBoundingClientRect();
+        if (!refContainer.current) return
+        const containerCoords = refContainer.current.getBoundingClientRect()
 
         setContainerPosition({
             top: containerCoords.top + pageYOffset,
             left: containerCoords.left + pageXOffset,
-        });
+        })
     }
 
     const onMouseDownHandler = () => {
-        updateContainerPosition();
-        setSliderMode(MODE.MOVE);
+        updateContainerPosition()
+        setSliderMode(MODE.MOVE)
     }
 
     const onMouseMoveHandler: MouseEventHandler<HTMLDivElement>
-        = (e ) => onMoveHandler(e);
+        = (e ) => onMoveHandler(e)
 
     const onTouchMoveHandler: TouchEventHandler<HTMLDivElement>
         = (e) => {
-        onMoveHandler(e.touches[0]);
-    };
+        onMoveHandler(e.touches[0])
+    }
 
     const onMoveHandler = (e: React.Touch | React.MouseEvent) => {
         if (sliderMode === MODE.MOVE) {
-            if (!imagesWidth) return;
-            const X = e.pageX - containerPosition.left;
-            const newPosition = normalizeNewPosition(X, imagesWidth) / imagesWidth * 100;
+            if (!imagesWidth) return
+            const X = e.pageX - containerPosition.left
+            const newPosition = normalizeNewPosition(X, imagesWidth) / imagesWidth * 100
             onChangePercentPosition
                 ? onChangePercentPosition(newPosition)
-                : setDelimiterPosition(newPosition);
+                : setDelimiterPosition(newPosition)
         }
     }
 
-    useResizeFeel(updateContainerWidth, withResizeFeel);
+    useResizeFeel(updateContainerWidth, withResizeFeel)
 
     const onClickHandlers = {
         onMouseDown: onMouseDownHandler,
@@ -281,5 +287,5 @@ export default function BeforeAfterSlider({
                 </>
             )}
         </div>
-    );
+    )
 }
